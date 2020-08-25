b937ecca176c077ddc879a2747b2baee95d80b3d
Yoni Goriachnick <yonig770@gmail.com>
2020-08-25 22:45:19 +0300

    init

---
+++ UserPreview-ObjC/WebService/WebServiceUtils.h
@@ -0,0 +1,17 @@
+//
+//  WebServiceUtils.h
+//  UserPreview-ObjC
+//
+//  Created by Yonathan Goriachnick on 25/08/2020.
+//  Copyright © 2020 YGApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol WebServiceUtils <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UserPreview-ObjC/WebService/WebServiceUtils.m
@@ -0,0 +1,9 @@
+//
+//  WebServiceUtils.m
+//  UserPreview-ObjC
+//
+//  Created by Yonathan Goriachnick on 25/08/2020.
+//  Copyright © 2020 YGApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
